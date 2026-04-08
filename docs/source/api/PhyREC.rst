267ef2f52de774dbc905d6b553f3de8647f30f9a
aguimera <anton.guimera@csic.es>
2026-04-08 13:44:24 +0200

    add sphinx documentation

---
+++ docs/source/api/PhyREC.rst
@@ -0,0 +1,61 @@
+PhyREC package
+==============
+
+Submodules
+----------
+
+PhyREC.Calibration\_old module
+------------------------------
+
+.. automodule:: PhyREC.Calibration_old
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+PhyREC.ImageSequence module
+---------------------------
+
+.. automodule:: PhyREC.ImageSequence
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+PhyREC.PlotWaves module
+-----------------------
+
+.. automodule:: PhyREC.PlotWaves
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+PhyREC.SignalAnalysis module
+----------------------------
+
+.. automodule:: PhyREC.SignalAnalysis
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+PhyREC.SignalProcess module
+---------------------------
+
+.. automodule:: PhyREC.SignalProcess
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+PhyREC.SupportFunctions module
+------------------------------
+
+.. automodule:: PhyREC.SupportFunctions
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: PhyREC
+   :members:
+   :show-inheritance:
+   :undoc-members:
---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PhyREC'
+copyright = '2026, Anton Guimerà Brunet'
+author = 'Anton Guimerà Brunet'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",   # Google / NumPy style docstrings
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+]
+
+autosummary_generate = True
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
